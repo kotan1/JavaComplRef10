d91e27486528d77591a18709582c45b3fc07db04
kotan1 <stvn.frst@yandex.ru>
2019-07-16 17:55:11 +0300

    Initial commit

---
+++ src/ch02/listing01/Example.java
@@ -0,0 +1,12 @@
+package ch02.listing01;
+
+/*
+   This is a simple Java program.
+   Call this file "IfSample.java".
+*/
+class Example {
+   // Your program begins with a call to main().
+   public static void main(String args[]) {
+      System.out.println("This is a simple Java program.");
+   }
+}
